3e3b397c2dbc1487105418721d8be62ca92f7d5d
JonasBouscharain <bouscharain.jonas@hotmail.fr>
2015-11-30 12:04:24 +0100

    The snake is walking !

--- snake.js
+++ snake.js
@@ -36,17 +36,89 @@ app.service('GameBoard', function(){
 			};
 			countL++;
 		}
-		console.log(l);
 		return l;
 	},
-	this.move = function(board, line, col){
-		board[line].cells[col].used = true;
-		console.log(board);
+	this.moveOne = function(board, line, col, state){
+		
+		board[line].cells[col].used = state;
+		board[line].cells[col].style = state?"cell-used":"cell-unused";
+		console.log(board[line].cells[col].style);
+	},
+	this.moveAll = function(board, snake, direction){
+		last = snake.shift();
+		this.moveOne(board, last.line, last.col, false);
+		//snake.shift();
+
+		line = snake[snake.length-1].line;
+		col = snake[snake.length-1].col;
+		if(direction == "n")
+			line += 1;
+		else if(direction == "s")
+			line -= 1;
+		else if(direction == "e")
+			col += 1;
+		else if(direction == "w")
+			col -= 1;
+		//console.log(line, col);
+		this.moveOne(board, line, col, true);
+		snake.push(board[line].cells[col]);
+	},
+	this.initSnake = function(board, snake){
+		for(i = 0 ; i < snake.length ; i++){
+			this.moveOne(board, snake[i].line, snake[i].col, true);
+		}
 	}
 })
 
-app.controller('mainController', ['$scope', 'GameBoard', function($scope, GameBoard) {
+app.controller('mainController', ['$scope', 'GameBoard', '$interval', '$timeout', '$document', function($scope, GameBoard, $interval, $timeout, $document) {
+	$scope.direction = "e";
 	$scope.windowSize = GameBoard.windowSize();
 	$scope.gameBoard = GameBoard.getGameBoard();
-	GameBoard.move($scope.gameBoard, 10, 10);
+	$scope.snake = [
+		$scope.gameBoard[10].cells[5],
+		$scope.gameBoard[10].cells[6],
+		$scope.gameBoard[10].cells[7],
+		$scope.gameBoard[10].cells[8]
+	];
+	$scope.speed = 100;
+	$scope.move = true;
+
+	GameBoard.initSnake($scope.gameBoard, $scope.snake);
+
+	$interval(function(){
+		GameBoard.moveAll($scope.gameBoard, $scope.snake, $scope.direction);
+		$scope.move = true;
+	}, $scope.speed);
+
+	
+	$scope.goTo = function($event){
+		if($scope.move){
+			if($event.keyCode == 37){
+				if($scope.direction != "e"){
+					$scope.direction = "w";
+					$scope.move = false;
+				}
+			}
+			else if($event.keyCode == 38){
+				if($scope.direction != "n"){
+					$scope.direction = "s";
+					$scope.move = false;
+				}
+			}
+			else if($event.keyCode == 39){
+				if($scope.direction != "w"){
+					$scope.direction = "e";
+					$scope.move = false;
+				}
+			}
+			else if($event.keyCode == 40){
+				if($scope.direction != "s"){
+					$scope.direction = "n";
+					$scope.move = false;
+				}
+			}
+		}
+	};
+
+	$document.on('keyup', $scope.goTo);
 }]);
